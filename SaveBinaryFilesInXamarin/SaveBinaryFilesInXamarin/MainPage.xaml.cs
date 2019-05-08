3956bc2dd82e4aec1882c86027cd9e515ca9bed6
Filip Przychodni <pierwiastekz3@interia.pl>
2019-05-08 14:41:29 +0200

    Working version (tested only UWP)

--- SaveBinaryFilesInXamarin/SaveBinaryFilesInXamarin/MainPage.xaml.cs
+++ SaveBinaryFilesInXamarin/SaveBinaryFilesInXamarin/MainPage.xaml.cs
@@ -13,27 +13,20 @@ namespace SaveBinaryFilesInXamarin
     
     public partial class MainPage : ContentPage
     {
-        public int currentNumber;
-        public IFolder photosFolder;
-        public byte[] b;
+        
+        byte[] b;
         public MainPage()
         {
-            InitializeComponent();           
+            InitializeComponent();
+            startCrossMedia();
         }
-        
-        protected override async void OnAppearing()
+        public async void startCrossMedia()
         {
-            base.OnAppearing();
-            //creates folder where files will be stored
             await CrossMedia.Current.Initialize();
 
-            IFileSystem fileSystem = FileSystem.Current;
-            IFolder rootFolder = fileSystem.LocalStorage;
-            IFolder photosFolder = await rootFolder.CreateFolderAsync("Photos", PCLStorage.CreationCollisionOption.OpenIfExists);
-                      
         }
       
-        private async Task TakePhoto_ClickedAsync(object sender, EventArgs e)
+        private async void TakePhoto_ClickedAsync(object sender, EventArgs e)
         {
             if (!CrossMedia.Current.IsCameraAvailable)
             {
@@ -45,14 +38,19 @@ namespace SaveBinaryFilesInXamarin
             {
                 SaveToAlbum = false
             });
+            if (file1 == null)
+            {
+                return;
+            }
             imagefromLibraryorCamera.Source = ImageSource.FromStream(() => file1.GetStream());
-            byte[] b = new byte[file1.GetStream().Length];
-            await file1.GetStream().ReadAsync(b, 0, (int)b.Length);
-            currentNumber = 1;
+            byte[] b1 = new byte[file1.GetStream().Length];
+            await file1.GetStream().ReadAsync(b1, 0, (int)b1.Length);
+            b = b1;
         }
 
-        private async Task FromLibrary_ClickedAsync(object sender, EventArgs e)
+        private async void FromLibrary_ClickedAsync(object sender, EventArgs e)
         {
+
             if (!CrossMedia.Current.IsPickPhotoSupported)
             {
                 await DisplayAlert("Error", "Can not take an image from device Library", "Ok");
@@ -65,16 +63,22 @@ namespace SaveBinaryFilesInXamarin
                 return;
             }
             imagefromLibraryorCamera.Source = ImageSource.FromStream(() => file2.GetStream());
-            byte[] b = new byte[file2.GetStream().Length];
-            await file2.GetStream().ReadAsync(b, 0, (int)b.Length);
-            currentNumber = 2;
+            byte[] b2 = new byte[file2.GetStream().Length];
+            await file2.GetStream().ReadAsync(b2, 0, (int)b2.Length);
+            b = b2;
         }
 
-        private async Task SaveButton_ClickedAsync(object sender, EventArgs e)
+        private async void SaveButton_ClickedAsync(object sender, EventArgs e)
         {
+            //creates folder where files will be stored
+
+
+            IFileSystem fileSystem = FileSystem.Current;
+            IFolder rootFolder = fileSystem.LocalStorage;
+            IFolder photosFolder = await rootFolder.CreateFolderAsync("Photos", PCLStorage.CreationCollisionOption.OpenIfExists);
             IFile saveFile = await photosFolder.CreateFileAsync("Name", PCLStorage.CreationCollisionOption.ReplaceExisting);
             
-            if (b.Length!=0)
+            if (b!=null)
             {
                 //if we want to save binary data as text file wee need to convert it to Base64String because some values are imposible to save in UFT-8 etc.
                 string imageAsBytes = System.Convert.ToBase64String(b);
@@ -86,12 +90,18 @@ namespace SaveBinaryFilesInXamarin
                 await DisplayAlert("No photo selected", "First you need to choose the image to save", "Ok");
                 return;
             }
-            
+            folderPath.Text = photosFolder.Path;
         }
 
-        private async Task LoadButtom_ClickedAsync(object sender, EventArgs e)
+        private async void LoadButtom_ClickedAsync(object sender, EventArgs e)
         {
-            //It is just an example more advanced projects need to have some JSON or txt file with all the folders, paths and file names saved
+            //creates folder where files will be stored
+
+
+            IFileSystem fileSystem = FileSystem.Current;
+            IFolder rootFolder = fileSystem.LocalStorage;
+            IFolder photosFolder = await rootFolder.GetFolderAsync("Photos");
+            //It is just an example more advanced projects need to have some JSON or txt file with all the folders,folder paths and file names saved
             IFile loadFile= await photosFolder.GetFileAsync("Name");
             string loadedBinaryImage = await loadFile.ReadAllTextAsync();
             //now we need to reverse file from string to bytes
